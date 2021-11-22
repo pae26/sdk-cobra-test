103293b19d6c3f7d4300310ed2f12eac745396a2
Pae <songju19990426@gmail.com>
2021-11-22 11:44:43 +0900

    1password更新コマンド追加

---
+++ cmd/onepassword.go
@@ -0,0 +1,70 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/spf13/cobra"
+)
+
+var onepasswordFlags struct {
+	apply    bool
+	vault    string
+	title    string
+	filename string
+}
+
+// onepasswordCmd represents the onepassword command
+var onepasswordCmd = &cobra.Command{
+	Use:     "1password",
+	Aliases: []string{"op"},
+	Short:   "Update 1password item",
+	Long: `Update 1password item.
+Set [-v, -t, -f] options.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if onepasswordFlags.vault == "" || onepasswordFlags.filename == "" {
+			fmt.Println("ERROR: You must set [-v, -t, -f] options.")
+			fmt.Println("Show help with [-h] option.")
+			os.Exit(0)
+		}
+
+		updateOnepassword(onepasswordFlags.apply, onepasswordFlags.vault, onepasswordFlags.title, onepasswordFlags.filename)
+	},
+}
+
+func updateOnepassword(apply bool, vault string, title string, filename string) {
+	if apply {
+		if title != "" {
+			_, err := exec.Command("op", "create", "document", filename, "--vault", vault, "--title", title).Output()
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		} else {
+			_, err := exec.Command("op", "create", "document", filename, "--vault", vault).Output()
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		}
+	} else {
+		fmt.Println("DRY-RUN finished. Use -a option to apply.")
+		fmt.Printf("%-11s: %s\n", "vault", vault)
+		if title != "" {
+			fmt.Printf("%-11s: %s\n", "title", title)
+		} else {
+			fmt.Printf("%-11s: %s\n", "title", "(not specify)")
+		}
+		fmt.Printf("%-11s: %s\n", "file path", filename)
+	}
+}
+
+func init() {
+	rootCmd.AddCommand(onepasswordCmd)
+
+	onepasswordCmd.Flags().BoolVarP(&onepasswordFlags.apply, "apply", "a", false, "default: dry-run")
+	onepasswordCmd.Flags().StringVarP(&onepasswordFlags.vault, "vault", "v", "", "vault name")
+	onepasswordCmd.Flags().StringVarP(&onepasswordFlags.title, "title", "t", "", "title of item")
+	onepasswordCmd.Flags().StringVarP(&onepasswordFlags.filename, "file", "f", "", "file path defined token information")
+}
